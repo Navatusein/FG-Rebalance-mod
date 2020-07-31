321321a9826a5916c325158ef26e8f871ef50fd9
Navatusein <53920406+Navatusein@users.noreply.github.com>
2020-07-31 19:26:06 +0300

    Delete skilltreetweakdata.lua

--- hooks/skilltreetweakdata.lua
+++
@@ -1,8 +0,0 @@
-Hooks:PostHook(SkillTreeTweakData, "init", "skilltreetweakdata", function(self, tweak_data)
-	
-	table.insert(self.default_upgrades, "player_health_damage_reduction_3")
-
-	self.skills.frenzy[1].upgrades = {"player_max_health_reduction_1", "player_healing_reduction_2", "player_health_damage_reduction_3"}
-	self.skills.frenzy[2].upgrades = {"player_healing_reduction_2", "player_damage_dampener_close_contact_1"}
-
-end)
